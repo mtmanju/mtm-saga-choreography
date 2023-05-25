40baad1331f532651fe217acbe962854de182f12
Manjunath M T <itzmtmanju@gmail.com>
2023-05-25 20:31:33 +0530

    Initial commit

---
+++ src/main/java/com/mtm/examples/mtmsagachoreography/MtmSagaChoreographyApplication.java
@@ -0,0 +1,13 @@
+package com.mtm.examples.mtmsagachoreography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtmSagaChoreographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtmSagaChoreographyApplication.class, args);
+	}
+
+}
